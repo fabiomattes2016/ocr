1d14751a75e4daa2d53fa1f999693971a5927aff
Fabio Jesus da Silva Mattes <fabiomattes2007@gmail.com>
2024-09-27 16:56:45 -0300

    Update extract_coordinates.py

--- extract_coordinates.py
+++ extract_coordinates.py
@@ -2,17 +2,26 @@ import cv2
 
 def obter_coordenadas(evento, x, y, flags, parametros):
     if evento == cv2.EVENT_LBUTTONDOWN:
-        print(f"Coordenada inicial: ({x}, {y})")
+        print(f"Coordenada inicial: {x}, {y}")
         
         parametros['x_inicial'],
         parametros['y_inicial'] = x, y
     elif evento == cv2.EVENT_LBUTTONUP:
-        print(f"Coordenada final: ({x}, {y})")
+        print(f"Coordenada final: {x}, {y}")
         parametros['x_final'],
         parametros['y_final'] = x, y
         
-        cv2.rectangle(imagem, (parametros['x_inicial'], parametros['y_inicial']), (x, y), (0, 255, 0), 2)
-        cv2.imshow("Imagem", imagem)
+        x1, y1 = parametros['x_inicial'], parametros['y_inicial']
+        x2, y2 = parametros['x_final'], parametros['y_final']
+        
+        # x1, x2 = min(x1, x2), max(x1, x2)
+        # y1, y2 = min(y1, y2), max(y1, y2)
+        
+        print(f"{x1}, {x2}, {y1}, {y2}")
+        
+        # cv2.rectangle(imagem, (x1, y1), (x2, y2), color=(255,0,0), thickness=2)
+
+        # cv2.imshow("Imagem", imagem)
 
 
 coordenadas = {
@@ -28,6 +37,6 @@ cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 print(f"""Coordenadas selecionadas: 
-    ({coordenadas['x_inicial']}, {coordenadas['y_inicial']}) até
-    ({coordenadas['x_final']}, {coordenadas['y_final']})
+    {coordenadas['x_inicial']}, {coordenadas['y_inicial']} até
+    {coordenadas['x_final']}, {coordenadas['y_final']}
 """)
